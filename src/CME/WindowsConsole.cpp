4ae7d78a4d2f5c8f3ce49c8a43e99341d9f42980
CodeMouse179 <codemouse179@gmail.com>
2023-04-09 20:28:46 +0300

    fix

--- src/CME/WindowsConsole.cpp
+++ src/CME/WindowsConsole.cpp
@@ -4,11 +4,11 @@ namespace cme
 {
     bool WindowsConsole::WriteConsoleA(const std::string& s)
     {
-
+        return false;
     }
 
     bool WindowsConsole::WriteConsoleW(const std::wstring& s)
     {
-
+        return false;
     }
 }
